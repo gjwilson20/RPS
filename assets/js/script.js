a3e58d3b8d6a9f9d72f911f4760c1930136ff088
gjwilson20 <166746021+gjwilson20@users.noreply.github.com>
2024-09-22 14:00:55 +0000

    added in JS constants and possible choices

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * declare player choices and constants for dom elements
+ */
+const buttons = document.getElementsByClassName("control");
+const player score = document.getElementById("player-score")
+const computer score = document.getElementById("computer-score");
+const player image = document.getElementById("player-image");
+const computer image = document.getElementById("computer-image");
+const message = document.getElementById("messages");
+const choices = ["rock", "paper", "scissors"];
