0b7f623f9cc960e865417bd86b599c0f85d1f124
gjwilson20 <166746021+gjwilson20@users.noreply.github.com>
2024-09-22 14:14:44 +0000

    added event listeners to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,14 @@ const player image = document.getElementById("player-image");
 const computer image = document.getElementById("computer-image");
 const message = document.getElementById("messages");
 const choices = ["rock", "paper", "scissors"];
+
+/**
+ * Add event listeners for buttons
+ */
+
+for (let button of buttons) {
+    button.addEventListener("click", function()}
+    let player choice = this.get attribute ("data-choice");
+    play Game (playerChoice);
+});
+}
